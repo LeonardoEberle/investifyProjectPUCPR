dc7c466d8f9aeceb7f3b66c95214479d9f768fbe
Leonardo Eberle <leonardo.eberle@capiware.com>
2024-04-11 04:34:01 -0300

    att

--- obj/Debug/net8.0/Investify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Investify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Investify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e615bfa599409d4b668d695d70f182faf8619f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af280f2784ac2b8cf06299288294885a424ef49d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Investify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Investify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
